58bac129b9615a021bc6ac322a4ee4522c662313
Alisson <arteaga10alisson@gmail.com>
2023-09-14 13:49:34 -0500

    add timeout

--- src/main/java/com/aarteaga/productos/controllers/ProductoController.java
+++ src/main/java/com/aarteaga/productos/controllers/ProductoController.java
@@ -40,13 +40,13 @@ public class ProductoController {
 		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 		//producto.setPort(port);
 		
-		
+		/*
 		try {
 			Thread.sleep(2000L);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}*/
 		
 		/*
 		boolean ok = false;
