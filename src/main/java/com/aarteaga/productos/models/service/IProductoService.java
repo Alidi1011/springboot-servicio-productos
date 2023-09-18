a84a10ac21ef52235f6d2727474ce00a39420fb9
Alisson <arteaga10alisson@gmail.com>
2023-09-18 15:33:56 -0500

    add crud operations

--- src/main/java/com/aarteaga/productos/models/service/IProductoService.java
+++ src/main/java/com/aarteaga/productos/models/service/IProductoService.java
@@ -7,5 +7,6 @@ import com.aarteaga.productos.models.entity.Producto;
 public interface IProductoService {
 	public List<Producto> findAll();
 	public Producto findById(Long id);
-
+	public Producto save(Producto producto);
+	public void deleteById(Long id);
 }
